27acdd677b7703143631440be0432da4e75e8970
LarsDu <larrydu88@gmail.com>
2022-10-14 22:37:47 -0700

    Fixed aspect ratio issues and ray gradient.

--- src/camera.rs
+++ src/camera.rs
@@ -16,20 +16,19 @@ pub struct Camera{
 
 impl Default for Camera{
     fn default() -> Self{
-
+        let origin = Point::ZERO;
         let aspect_ratio: f32 = 16.0/9.0;
         let viewport_height = 2.0;
         let viewport_width = aspect_ratio * viewport_height;
-        let origin = Point::ZERO;
         let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
         let vertical = Vec3::new(0.0, viewport_height, 0.0);
         let focal_length= 1.0;
         Camera { 
+            origin: origin, 
             aspect_ratio: aspect_ratio, 
             viewport_height: viewport_height, 
             viewport_width: viewport_width, 
             focal_length: focal_length, 
-            origin: origin, 
             horizontal: horizontal,
             vertical: vertical,
             lower_left_corner: origin - horizontal * 0.5 - vertical * 0.5 - Vec3::new(0.0, 0.0, focal_length)
--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,31 @@ mod camera;
 use camera::Camera;
 fn main() {
     // Image
-    const aspect_ratio: f32 = 16.0/9.0;
-    const image_width: u32 = 400;
-    const image_height: u32 = image_width/aspect_ratio as u32;
+    const ASPECT_RATIO: f32 = 16.0/9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f32/ASPECT_RATIO) as u32;
+    eprintln!("Image width: {IMAGE_WIDTH}\tImage height{IMAGE_HEIGHT}");
     // Camera
     let camera = Camera::default();
     // Render
-    render(image_width, image_height, &camera);
+    render(IMAGE_WIDTH, IMAGE_HEIGHT, &camera);
 }
 
-fn ray_color(ray: &Ray) -> Color {
+fn hit_sphere(center: Point, radius: f32, r: &Ray) -> bool{
+    let oc: Vec3 = r.origin - center;
+    let a: f32 = Vec3::dot_product(&r.direction, &r.direction);
+    let b = Vec3::dot_product(&oc, &r.direction) * 2.0;
+    let c: f32 = Vec3::dot_product(&oc, &oc) - radius * radius;
+    let discriminant = b*b - 4.0*a*c;
+    return discriminant > 0.0;
+}
+
+fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point::new(0.0, 0.0, -1.0), 0.5, &r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     // Calculate unit direction
-    let unit_direction: Vec3 = Vec3::normalized(ray.direction);
+    let unit_direction: Vec3 = Vec3::normalized(r.direction);
     let t: f32 = 0.5 * (unit_direction.y + 1.0);
     return Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t;
 }
@@ -34,10 +47,10 @@ fn render(image_width: u32, image_height: u32, camera: &Camera) {
     println!("{image_height}");
     println!("255\n");
     for row in (0..image_height).rev() {
-        eprintln!("Rows remaining: {}", image_height - row);
+        eprintln!("Rows remaining: {}", row);
         for col in 0..image_width {
-            let u: f32 = (row as f32) / (image_width as f32 - 1.0);
-            let v: f32 = (col as f32) / (image_height as f32 - 1.0);
+            let u: f32 = (col as f32) / (image_width as f32 - 1.0);
+            let v: f32 = (row as f32) / (image_height as f32 - 1.0);       
             // Cast a ray for every pixel in the camera
             let r = Ray::new(
                 camera.origin,
